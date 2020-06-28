62e48996d790840981133aedf796df154ab0ab80
Ognjen <opejovic@gmail.com>
2020-06-28 14:00:00 +0200

    Create Model Factories

--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -2,11 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Models\User;
 use App\Models\Thread;
 use Faker\Generator as Faker;
 
 $factory->define(Thread::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'title' => $faker->sentence($words = 3),
+        'body' => $faker->realText($maxChars = 300)
     ];
 });
