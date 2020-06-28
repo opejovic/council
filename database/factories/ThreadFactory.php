a2a581ec87bbb3e5669d071fa30c559c8b2908ed
Ognjen <opejovic@gmail.com>
2020-06-28 13:48:01 +0200

    Create Thread Resource

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Thread;
+use Faker\Generator as Faker;
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
