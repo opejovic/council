46a9a5aa79a8feda13009a200ae63afa1a6a0532
Ognjen <opejovic@gmail.com>
2021-08-04 11:51:21 +0200

    formatting

--- resources/js/components/RepliesComponent.vue
+++ resources/js/components/RepliesComponent.vue
@@ -1,50 +1,47 @@
 <template>
-    <div>
-        <div v-if="items.length > 0">
-            <div v-for="item in items" :key="item.id">
-                <reply-component :attributes="item" @deleted="refreshReplies" />
-            </div>
-        </div>
-
-        <div v-else>
-            No replies yet.
-        </div>
+  <div>
+    <div v-if="items.length > 0">
+      <div v-for="item in items" :key="item.id">
+        <reply-component :attributes="item" @deleted="refreshReplies" />
+      </div>
     </div>
+
+    <div v-else>No replies yet.</div>
+  </div>
 </template>
 
 <script>
-    import ReplyComponent from "./ReplyComponent";
+import ReplyComponent from "./ReplyComponent";
 
-    export default {
-        name: 'RepliesComponent',
+export default {
+  name: "RepliesComponent",
 
-        components: {
-            ReplyComponent,
-        },
+  components: {
+    ReplyComponent,
+  },
 
-        props: {
-            replies: {
-                type: Object,
-                default: null
-            },
-        },
+  props: {
+    replies: {
+      type: Object,
+      default: null,
+    },
+  },
 
-        data() {
-            return {
-                items: this.replies.data
-            }
-        },
+  data() {
+    return {
+      items: this.replies.data,
+    };
+  },
 
-        methods: {
-            refreshReplies(e) {
-                return this.items = this.items.filter(function (item) {
-                    return item.id !== e;
-                });
-            }
-        },
-    }
+  methods: {
+    refreshReplies(e) {
+      return (this.items = this.items.filter(function (item) {
+        return item.id !== e;
+      }));
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
